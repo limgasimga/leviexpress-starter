b7ea0c46365a9010e32f14576c8829d92dce0fba
limgasimga <simonabohusova@gmail.com>
2021-05-29 10:48:59 +0200

    Part 2

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from "../Reservation";
 
 export const App = () => (
-  <>
+  <Router>
     <Header />
-    <Home />
+    <Switch>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route path="/reservation">
+        <Reservation />
+      </Route>
+      </Switch>
     <Footer />
-  </>
+  </Router>
 );
